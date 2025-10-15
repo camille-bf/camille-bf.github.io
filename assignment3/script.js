6e551ddb57a6016f066b40506ab86a118e12234f
camille-bf <s4193646@student.rmit.edu.au>
2025-10-15 19:14:17 +1100

    removed navbar, added progress bar

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,1 +1,7 @@
-
+const audio = document.getElementById("audioPlayer");
+const progressBar = document.querySelector("#progress-bar-fill");
+audio.addEventListener("timeupdate", updateProgress);
+function updateProgress() {
+  let duration = (audio.currentTime / audio.duration) * 100;
+  progressBar.style.width = duration + "%";
+}
